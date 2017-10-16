63791102806b47364fa13c545efe88d614b4ad4f
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-10-16 14:53:30 -0400

    fixing insta

--- sites/instagram.js
+++ sites/instagram.js
@@ -1,5 +1,5 @@
 oAuthMethods.instagram = function(){
-	console.log('log into instagram with:' + 'client_id: 4f006fb6e156494d9f84141085c9ddae, client_secret: 8c233ead378f4ec399226e6ff988ea3b, request_token: https://api.instagram.com/oauth/authorize/?client_id=4f006fb6e156494d9f84141085c9ddae&redirect_uri=https://kaseyllabelle.github.io/IoP/callback.html&response_type=token);
+	console.log('log into instagram with:' + 'client_id: 4f006fb6e156494d9f84141085c9ddae, client_secret: 8c233ead378f4ec399226e6ff988ea3b, request_token: https://api.instagram.com/oauth/authorize/?client_id=4f006fb6e156494d9f84141085c9ddae&redirect_uri=https://kaseyllabelle.github.io/IoP/callback.html&response_type=token');
 	// 'instagram': {
 	// 	client_id: '4f006fb6e156494d9f84141085c9ddae',
 	// 	client_secret: '8c233ead378f4ec399226e6ff988ea3b',

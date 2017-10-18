910dd21e774706c72457b4421aa86c093ea4431f
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-10-18 16:01:25 -0400

    testing fivehundredpx

--- sites/fivehundredpx.js
+++ sites/fivehundredpx.js
@@ -1,7 +1,7 @@
 oAuthMethods.fivehundredpx = function(obj, requestFinalToken = false){
 	console.log(obj);
 	if(!requestFinalToken){
-		window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=04a57d5e6d2c3a8ee908e4c41db09f0f5ec31b26&callback=oAuthMethods.fivehundredpxcb',
+		window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=d53a58c94138c694793ca71c78cf347dae4b7e1b&callback=oAuthMethods.fivehundredpxcb',
 						'500px_js_sdk_login',
 						'width=1240,height=480,left=100,top=100,menu=no,location=yes,scrollbars=no,status=no,toolbar=no');
 	}
@@ -15,7 +15,7 @@ oAuthMethods.fivehundredpx = function(obj, requestFinalToken = false){
 oAuthMethods.fivehundredpxcb = function(obj){
 	console.log(obj);
 	let iframe_element = document.createElement('iframe');
-	iframe_element.src = 'https://api.500px.com/api/js-sdk/check_authorization?sdk_key=04a57d5e6d2c3a8ee908e4c41db09f0f5ec31b26&callback=check500';
+	iframe_element.src = 'https://api.500px.com/api/js-sdk/check_authorization?sdk_key=d53a58c94138c694793ca71c78cf347dae4b7e1b&callback=check500';
 	$('body').appendChild(iframe_element);
 };
 

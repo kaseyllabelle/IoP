cf239b517ba261fbe9965091a1f42beaaabbfdee
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-10-18 16:21:21 -0400

    testing fivehundredpx

--- sites/fivehundredpx.js
+++ sites/fivehundredpx.js
@@ -1,14 +1,16 @@
 oAuthMethods.fivehundredpx = function(obj, requestFinalToken = false){
 	console.log(obj);
+	let win;
 	if(!requestFinalToken){
-		window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=d53a58c94138c694793ca71c78cf347dae4b7e1b&callback=oAuthMethods.fivehundredpxcb',
+		win = window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=d53a58c94138c694793ca71c78cf347dae4b7e1b&callback=oAuthMethods.fivehundredpxcb',
 						'500px_js_sdk_login',
 						'width=1240,height=480,left=100,top=100,menu=no,location=yes,scrollbars=no,status=no,toolbar=no');
 	}
 	else{
 		console.log('paused');
-		// localStorage.setItem('fivehundredpx_token', getURLParameter('access_token'));
-		// window.location.href = '/IoP/callback.html';
+		localStorage.setItem('fivehundredpx_token', getURLParameter('token:'));
+		win.close();
+		oAuthMethods.fivehundredpxcb();
 	}
 };
 
@@ -21,6 +23,7 @@ oAuthMethods.fivehundredpxcb = function(obj){
 
 oAuthMethods.fivehundredpxComplete = function(obj){
 	console.log(obj);
+	// location.href = '/IoP/callback.html';
 };
 
 

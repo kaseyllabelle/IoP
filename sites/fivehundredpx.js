9ff4db38c4cd9ffb268a9e9651879c832ceff0d6
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-10-18 15:49:06 -0400

    testing fivehundredpx

--- sites/fivehundredpx.js
+++ sites/fivehundredpx.js
@@ -1,6 +1,12 @@
-oAuthMethods.fivehundredpx = function(){
-	console.log('log into 500px with:' + 'https://api.500px.com/v1/oauth/request_token?oauth_callback=https://kaseyllabelle.github.io/IoP/callback.html');
-	// 'fivehundredpx': {
-	// 	post: 'https://api.500px.com/v1/oauth/request_token?oauth_callback=https://kaseyllabelle.github.io/IoP/callback.html'
-	// }
+oAuthMethods.fivehundredpx = function(obj, requestFinalToken = false){
+	if(!requestFinalToken){
+		$.ajax({
+			method: 'post',
+			url: 'https://api.500px.com/v1/oauth/request_token?oauth_callback=https://kaseyllabelle.github.io/IoP/callback.html'
+		})
+	}
+	else{
+		localStorage.setItem('fivehundredpx_token', getURLParameter('access_token'));
+		window.location.href = '/IoP/callback.html';
+	}
 };
--- sites/instagram.js
+++ sites/instagram.js
@@ -6,10 +6,4 @@ oAuthMethods.instagram = function(obj, requestFinalToken = false){
 		localStorage.setItem('instagram_token', getURLParameter('access_token'));
 		window.location.href = '/IoP/callback.html';
 	}
-	// 'instagram': {
-	// 	client_id: '4f006fb6e156494d9f84141085c9ddae',
-	// 	client_secret: '8c233ead378f4ec399226e6ff988ea3b',
-	// 	request_token: 'https://api.instagram.com/oauth/authorize/?client_id=4f006fb6e156494d9f84141085c9ddae&redirect_uri=https://kaseyllabelle.github.io/IoP/callback.html&response_type=token'
-	// 	// 'https://kaseyllabelle.github.io/IoP/callback.html#access_token=6103829376.4f006fb.ae8af2aa30394f38b996aaf57002d7e4'
-	// }
 };

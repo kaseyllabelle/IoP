af4e22252dae300921460f15a1dac7b4d6f0143b
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-10-20 15:02:51 -0400

    testing 500px

--- sites/fivehundredpx.js
+++ sites/fivehundredpx.js
@@ -8,13 +8,19 @@ oAuthMethods.fivehundredpx = function(obj, requestFinalToken = false){
 		console.log(location.href);
 		let token = location.href.replace(/.*token:/g,'').replace(/,callback:.*/g,'');
 		localStorage.setItem('fivehundredpx_token', token);
-		win.close();
+
+		window.close(),
+		self.close(),
+		var win = window.open("","_self"); win.close();
+		window.parent.close();
+		top.open('','_self',''); top.close();
 	}
 
 	if(!requestFinalToken){
 		win = window.open('https://api.500px.com/api/js-sdk/authorize?sdk_key=d53a58c94138c694793ca71c78cf347dae4b7e1b&callback=oAuthMethods.fivehundredpxcb',
 						'500px_js_sdk_login',
 						'width=1240,height=480,left=100,top=100,menu=no,location=yes,scrollbars=no,status=no,toolbar=no');
+		// recursive function that checks if token is working and then closes window
 	}
 	else{
 		console.log('paused!');

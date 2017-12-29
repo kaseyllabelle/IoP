451eaaf970101f5f91b8a9ffc001116a7913f172
Kasey Labelle <kasey.l.labelle@gmail.com>
2017-12-29 01:04:27 -0500

    wip

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ $(document).ready(function(){
 		window.location.href = '/IoP/callback.html';
 	});
 
- //  	// remove this before pushing
-	// localStorage.setItem('fivehundredpx_token', 'jdqPAbiJ7hoWo9IAuM0DjNS23P1mt2VOAc3p1x7T');
-	// localStorage.setItem('giphy_token', 'AmvMPkwbW7v9oeXFDjyRXtCWJtNOZ2UL');
-	// localStorage.setItem('imgur_token', '25106fb1b49ed4ba17901c415d3c2ac8803ec921');
-	// localStorage.setItem('instagram_token', '6103829376.4f006fb.ae8af2aa30394f38b996aaf57002d7e4');
-	// // localStorage.setItem('pinterest_token', 'AeGLHsdraOcQzJFOXIzGOuthtj9iFQJnit7vt0pEkOfdp8A4swAAAAA');
-	// localStorage.setItem('tumblr_token', 'NPVdky515PEendzYYA44WarFcZeKhstduONQB979h3q8KMFCKM');
-	// localStorage.setItem('youtube_token', 'AIzaSyCzk-5OigvpFn0Bo1U8InpPOj5VOc3Awf8');
-	// localStorage.setItem('query', 'pittie');
+  	// remove this before pushing
+	localStorage.setItem('fivehundredpx_token', 'jdqPAbiJ7hoWo9IAuM0DjNS23P1mt2VOAc3p1x7T');
+	localStorage.setItem('giphy_token', 'AmvMPkwbW7v9oeXFDjyRXtCWJtNOZ2UL');
+	localStorage.setItem('imgur_token', '25106fb1b49ed4ba17901c415d3c2ac8803ec921');
+	localStorage.setItem('instagram_token', '6103829376.4f006fb.ae8af2aa30394f38b996aaf57002d7e4');
+	// localStorage.setItem('pinterest_token', 'AeGLHsdraOcQzJFOXIzGOuthtj9iFQJnit7vt0pEkOfdp8A4swAAAAA');
+	localStorage.setItem('tumblr_token', 'NPVdky515PEendzYYA44WarFcZeKhstduONQB979h3q8KMFCKM');
+	localStorage.setItem('youtube_token', 'AIzaSyCzk-5OigvpFn0Bo1U8InpPOj5VOc3Awf8');
+	localStorage.setItem('query', 'pittie');
 
 	if(~window.location.href.indexOf('load-puppies')){
 		oAuthMethods.compiledImages = [];
@@ -66,9 +66,10 @@ let h = 0;
 function imageLoaded(){
 	if(++h >= oAuthMethods.compiledImages.length){
 		var msnry = new Masonry( elem, {
-			// options
 			itemSelector: '.grid-item',
-			columnWidth: 200
+			columnWidth: '.grid-sizer',
+			gutter: '.gutter-sizer',
+			percentPosition: true
 		});
 		$('.grid').css("visibility", "visible");
 	}
@@ -82,11 +83,11 @@ oAuthMethods.displayImages = function(){
 	for(i=0; i<oAuthMethods.compiledImages.length; i++){
 		$('.grid').append(`
 			<div class="grid-item wrapper">
-				<a href="${oAuthMethods.compiledImages[i].url}">
+				<a href="${oAuthMethods.compiledImages[i].url}" target="_blank">
 					<div class="wrapper-inner">
-						<img onLoad="imageLoaded()" class="puppy-pic" src="${oAuthMethods.compiledImages[i].thumbnail}"/>
+						<img onLoad="imageLoaded()" class="thumbnail" src="${oAuthMethods.compiledImages[i].thumbnail}"/>
 					</div>
-					<p class="title">Source: ${oAuthMethods.compiledImages[i].source}</p>
+					<p class="caption">Source: ${oAuthMethods.compiledImages[i].source}</p>
 				</a>
 			</div>
 		`);
